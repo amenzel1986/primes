5b3d71b15b4040a6026d4cbb573f5ddb1671ab41
amenzel1986 <andrew.menzel1986@gmail.com>
2015-04-16 12:57:43 -0400

    fixed indent

--- primes.py
+++ primes.py
@@ -1,12 +1,12 @@
 from datetime import datetime
 start = datetime.now()
 primes = [2]
-for n in range (3, 10001):
+for n in range (3, 100001):
   for p in primes[:len(primes)/2]:
     rem = n%p
     if rem == 0:
       break
-    else:
+  else:
       primes += [n]
 print len(primes)
 print datetime.now() - start
